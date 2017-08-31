d326200b4226ddb665cbd211dac48d070dbbfd82
Pepe Valdivia <jvaldivia@softweb.pe>
2017-08-31 12:56:51 -0500

    Set Header como hook antes de cualquier metodo de cada handler

--- lib/Handler/App.pm
+++ lib/Handler/App.pm
@@ -3,15 +3,16 @@ use Dancer2;
 
 our $VERSION = '0.1';
 
+hook before => sub {
+    response_header 'X-Powered-By' => 'Perl Dancer 1.3202, Ubuntu';
+  };
+
 get '/' => sub {
     template 'index' => { 'title' => 'Perl Dancer2 ????' };
 };
 
 get '/hola' => sub {
-	response_header 'X-Powered-By' => 'Perl Dancer 1.3202';
     return 'hola mundo!';
 };
-
-#get  '/login'        => \&Handler::Login::index;
 
 true;
--- lib/Handler/Login.pm
+++ lib/Handler/Login.pm
@@ -1,8 +1,17 @@
 package Handler::Login;
 use Dancer2;
 
+hook before => sub {
+    response_header 'X-Powered-By' => 'Perl Dancer 1.3202, Ubuntu';
+  };
+
+get '/anda' => sub {
+	#response_header 'X-Powered-By' => 'Perl Dancer 1.3202';
+    return 'hola mundo!';
+};
+
 get '/' => sub {
-	response_header 'X-Powered-By' => 'Perl Dancer 1.3202';
+	#response_header 'X-Powered-By' => 'Perl Dancer 1.3202';
     return 'hola mundo!';
 };
 
