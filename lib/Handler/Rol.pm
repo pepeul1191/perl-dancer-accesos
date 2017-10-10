e79e008b067a62884e87727dbdfc9d2ab4f7a074
Pepe Valdivia <jvaldivia@softweb.pe>
2017-10-10 11:39:31 -0500

    CRUD de Roles con UTF8

--- lib/Handler/Rol.pm
+++ lib/Handler/Rol.pm
@@ -7,6 +7,8 @@ use Data::Dumper;
 use Try::Tiny;
 use strict;
 use warnings;
+use utf8;
+use Encode qw( encode_utf8 );
 
 get '/listar/:sistema_id' => sub {
     my $model = 'Model::Rol';
@@ -18,7 +20,7 @@ get '/listar/:sistema_id' => sub {
 };
 
 post '/guardar' => sub {
-    my $data = decode_json(param('data'));
+    my $data = decode_json(encode_utf8(param('data')));
     my @nuevos = @{$data->{"nuevos"}};
     my @editados = @{$data->{"editados"}};
     my @eliminados = @{$data->{"eliminados"}};
@@ -88,7 +90,7 @@ sub eliminar {
 }
 
 post '/asociar_permisos' => sub {
-    my $data = decode_json(param('data'));
+    my $data = decode_json(encode_utf8(param('data')));
     my @nuevos = @{$data->{"nuevos"}};
     my @editados = @{$data->{"editados"}};
     my @eliminados = @{$data->{"eliminados"}};
